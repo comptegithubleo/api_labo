64354113ac343a7d53b6f247b350c61c30c8a20a
oue <bleumisc@gmail.com>
2025-09-30 17:04:48 +0200

    ADDed a malware to the project

---
+++ router/scripts/createUser.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# get the smallest vlan id that's not yet registered in interfaces
+vlan_id=$(grep -oE 'vlan[0-9]{1,4}' /etc/network/interfaces \
+  | grep -oE '[0-9]{1,4}' \
+  | sort -un \
+  | awk 'BEGIN{min=10;found=0} {if($1==min) min++; else if($1>min){print min; found=1; exit}} END{if(!found) print min}')
+
+echo $vlan_id
+
+wg_ip=$(printf "10.%d.%d.0" $((vlan_id / 100)) $((vlan_id % 100)))
+
+echo $wg_ip
